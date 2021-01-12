e702ad12f1a87f45f9e43f34d414dcf7bd264d3d
diegosqz <contato.diegosq@gmail.com>
2021-01-11 22:04:20 -0300

    ok

--- Model/users.js
+++ Model/users.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const UsersSchema = new Schema ({
+const bcrypt = require('bcrypt');
+const UsersSchema = new Schema ({    
     email: { type: String}, required: true, unique: true, lowercase: true ,
     password: { type: String, required: true, select: false },
     create: {type: Date, default:Date.now}
-}) 
+}); 
     //select: false fala sobre a senha não retornar nas pesquisas realizadas.
     //o tipo vai ser de palavras, será algo que não pode ficar sem fazer e será em minusculo
 
+    UsersSchema.pre('save', function (next){
+        let users = this;
+        if (!users.isModified('password')) return next();
+    
+        bcrypt.hash(users.password, 10, (err, encrypted) => {
+        users.password = encrypted;
+        return next();
+        });
+    });
+        
 
 module.exports = mongoose.model('Users', UsersSchema);
