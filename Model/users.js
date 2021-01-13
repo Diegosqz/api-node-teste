d3a2ea082a7c8ca6a6cc9dde14f24830d810a89c
diegosqz <contato.diegosq@gmail.com>
2021-01-13 19:15:39 -0300

    evolução

--- Model/users.js
+++ Model/users.js
@@ -6,18 +6,26 @@ const UsersSchema = new Schema ({
     password: { type: String, required: true, select: false },
     create: {type: Date, default:Date.now}
 }); 
-    //select: false fala sobre a senha não retornar nas pesquisas realizadas.
-    //o tipo vai ser de palavras, será algo que não pode ficar sem fazer e será em minusculo
-
-    UsersSchema.pre('save', function (next){
-        let users = this;
-        if (!users.isModified('password')) return next();
-    
-        bcrypt.hash(users.password, 10, (err, encrypted) => {
-        users.password = encrypted;
-        return next();
-        });
-    });
         
+    UsersSchema.pre('save', async function (next){
+        let user = this;
+        if (!user.isModified('password')) return next();
+        UsersSchema.password = await bcrypt.hash(users.password, 10);
+        return next();
+    });
+
 
 module.exports = mongoose.model('Users', UsersSchema);
+
+
+        //select: false fala sobre a senha não retornar nas pesquisas realizadas.
+        //o tipo vai ser de palavras, será algo que não pode ficar sem fazer e será em minusculo
+        //Refatorando com async await
+        //UsersSchema.pre('save', function (next){
+        //let users = this;
+        //if (!users.isModified('password')) return next();
+        //bcrypt.hash(users.password, 10, (err, encrypted) => {
+        //users.password = encrypted;
+        //return next();
+        //});
+        //});
--- Routes/users.js
+++ Routes/users.js
@@ -1,38 +1,92 @@
 const express = require ('express');
 const router = express.Router();
 const Users = require('../Model/users');
+const bcrypt = require('bcrypt');
 
-
-router.get('/', (req, res) => {
-    Users.find({}, (err, data) => {
-        if (err) return res.send({error: 'Erro de consulta de usuarios'});
-        return res.send(data);
-
+    router.get('/', async(req, res) => 
+    {   
+        try {
+                const users = await Users.find({});
+                return res.send(users);
+        }
+        catch (err) {
+            return res.send ({error: 'Erro na consulta de usuários!'});
+        }
     });
-    
+
+
+    router.post('/create', async (req, rest) => {
+        const {email, password} = req.body;
+            if(!email || !password) return res.send({ error: 'Dados insuficientes!'});
+    try{
+            if (await Users.findOne({ email})) return res.send({error: 'Usuário já registrado!'});
+            const users = await Users.create(req.body);
+            users.passoword = undefined;
+            return res.send(users);
+    }
+    catch (err) {
+            return res.send({error: 'Erro ao buscar usuário!'});
+    }
 });
 
 
 
-router.post('/create', (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) return res.send({error: 'Dados insuficientes'});
-    Users.findOne({email}, (err, data) => {
-        if (err) return res.send({error: 'Erro ao buscar usuário'});
-        if (data) return res.send({error: 'Usuario já registrado'});
-    
-        
-    Users.create(req.body, (err, data) => {
-        if (err) return res.send({ error: 'Erro ao criar usuário'});
-        data.password = undefined;
-        return res.send(data);
-    });
-    
-    });
-
+router.post('/auth', async (req, res) =>{
+    const {email, password} = req.body;
+    if(!email || !password) return res.send({error: 'Dados insuficientes!'});
+    try{
+        const users = await Users.findOne({email}).select('+password');
+        if(!users) return res.send({ error: 'Usuário não registrado!'});
+        const pass_ok = await bcrypt.compare(password, users.passoword);
+        if(!pass_ok) return res.send ({error: 'Erro ao buscar usuário'});
+        users.passoword = undefined;
+        return res.send(users);
+    }
+    catch (err) {
+        return res.send({ error: 'Erro ao buscar usuário'});
+    }
 });
 module.exports = router;
 
+
+
+//Refatorando a rota get com async await
+//router.get('/', (req, res) => {
+//Users.find({}, (err, data) => {
+//if (err) return res.send({error: 'Erro de consulta de usuarios'});
+//return res.send(data);
+//});
+
+//Refatorado a rota create com async e await
+//router.post('/create', (req, res) => {
+//const { email, password } = req.body;
+//if (!email || !password) return res.send({error: 'Dados insuficientes'});
+//Users.findOne({email}, (err, data) => {
+//if (err) return res.send({error: 'Erro ao buscar usuário'});
+//if (data) return res.send({error: 'Usuario já registrado'});        
+//Users.create(req.body, (err, data) => {
+//if (err) return res.send({ error: 'Erro ao criar usuário'});
+//data.password = undefined;
+//return res.send(data);
+//}); 
+//});
+//});
+
+
+//Refatorando a rota auth com async e await
+//router.post('/auth', (req, res) =>{
+//const{email, password} = req.body;
+//if(!email || !passoword) return res.send({ error: 'Dados insuficientes!'});
+//Users.findOne({email}, (err, data) => {
+//if (err) return res.send({ error: 'Erro ao buscar usuário!'});
+//if (!data) return res.send({errror: 'Usuário não registrado'});
+//bcrypt.compare(password, data.passoword, (err, same ) => {
+//if (!same) return res.send({error: 'Erro ao autenticar usuário'});
+//data.passoword = undefined;
+//return res.send(data);
+//})
+//}).select('+password');
+//});
 //return res.send({message: 'Bateu tudo ok aqui'});
 //router.post('/', (req, res) => {
 //return res.send({message: 'Tudo ok aqui tbm'})
